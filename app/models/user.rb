d13074b13476043a35eb6588612fa56b1984cc16
grace-mugoiri <gracemugoiri@gmail.com>
2019-07-08 17:21:03 +0300

    implemented edit and update methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     validates :username, presence: true, length: { maximum: 50}
     valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255}
-    validates :password, presence: true, length: { maximum: 16, minimum:8}
+    validates :password, presence: true, length: { maximum: 16, minimum:8}, format: {with: valid_email_regex}, uniqueness: {case_sensitive: false}
 end
